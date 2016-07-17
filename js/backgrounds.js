800a75d76aaac41ebaae77576e596e1c5eb887c0
Eklavya Sharma <ekurgn@gmail.com>
2016-07-18 01:48:25 +0530

    Increase speed a lot and decrease pakias.

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -97,12 +97,12 @@
     },
 
     resetAllSpeed: function() {
-        this.cloud_bg_move_speed = 2;
-        this.backtree_bg_move_speed = 3;
-        this.fronttree_bg_move_speed = 5;
-        this.ground_bg_move_speed = 7;
+        this.cloud_bg_move_speed = 4;
+        this.backtree_bg_move_speed = 6;
+        this.fronttree_bg_move_speed = 10;
+        this.ground_bg_move_speed = 14;
 
-        this.combined_bg_move_speed = 3;
+        this.combined_bg_move_speed = 6;
     },
 
     drawClouds: function(ctx) {
